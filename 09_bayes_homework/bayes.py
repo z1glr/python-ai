fa50bd7a549a534569c7c8ff7d2c7584a5b5a1a2
Simon Ziegler <simon.ziegler.1998@gmail.com>
2023-11-02 15:29:34 +0100

    Rewrite with math-fixing and formatting

--- 09_bayes_homework/bayes.py
+++ 09_bayes_homework/bayes.py
@@ -1,41 +1,75 @@
+"""Implementation of the bayes classificator"""
 import pandas as pd
 
 class NaiveBayes:
+    """Implementation of a naive bayes classificator"""
+    # the dataset used to predict results
     df_data: pd.DataFrame
+    # column-names for the result-columns
     lst_s_results: list[str]
-    
-    dct_f_products: dict[float]
 
+    # alpha-factor for the laplace-correction
     f_laplace_alpha: float
 
     def __init__(self, df_data: pd.DataFrame, lst_s_results: list[str], f_laplace_alpha: float = 1):
+        """initializing function
+
+        Args:
+            df_data (pd.DataFrame): dataframe for predicting the results
+            lst_s_results (list[str]): column-names for the result-columns
+            f_laplace_alpha (float, optional): factor for the laplace-correction. Defaults to 1.
+        """
         self.df_data = df_data
         self.lst_s_results = lst_s_results
         self.f_laplace_alpha = f_laplace_alpha
 
     def f_predict(self, str_result: str, sr_data: pd.Series, state=True) -> float:
-        f_res_a = self.f_prop_P_product(sr_data, str_result, state)
-        f_res_b = 0.0
+        """predicts the propability for a result for the given input states
 
-        lst_results = self.lst_s_results.copy()
-        lst_results.pop(self.lst_s_results.index(str_result))
+        Args:
+            str_result (str): name of the result column
+            sr_data (pd.Series): input-data
+            state (bool, optional): state of the result to predict for. Defaults to True.
 
-        for rr in lst_results:
-            f_res_b += self.f_prop_P_product(sr_data, rr, state)
+        Returns:
+            float: propability for the result given the input states
+        """
+        # calculate the prior-propabilty
+        f_prior = self.df_data[str_result].mean()
+        # calculate the prior * likelihood
+        f_res_a = f_prior * self.f_likelihood(str_result, sr_data, state)
+        # calculate the part of the evidence
+        f_res_b = (1 - f_prior) * self.f_likelihood(sr_data, str_result, not state)
 
+        # calculate the naive-bayes
         return 1 / (1 + f_res_b / f_res_a)
 
-    def f_prop_P_product(self, sr_data: pd.Series, str_result: str, state=True) -> float:
-        df_subdata = self.df_data[self.df_data[str_result] == state]
-        f_result = self.df_data[str_result].mean()
+    def f_likelihood(self, str_result: str, sr_data: pd.Series, state=True) -> float:
+        """calculate the likelihood of a result given the input states
 
+        Args:
+            str_result (str): name of the result column
+            sr_data (pd.Series): input-data
+            state (any, optional): state of the result to predict for. Defaults to True.
+
+        Returns:
+            float: likelihood of the result given the input states
+        """
+        # select only the data, where the result is of the state we are looking for
+        df_subdata = self.df_data[self.df_data[str_result] == state]
+        f_result = 1
+
+        # go through the individual input-states
         for pp in sr_data.items():
+            # initialize with the laplace-alpha
             i_count = self.f_laplace_alpha
 
+            # check, wether the value even exists in the data
             if pp[1] in df_subdata[pp[0]].values:
+                # add the number of occurences to the counter
                 i_count += df_subdata[pp[0]].value_counts()[pp[1]]
 
-            zwischen = i_count / len(df_subdata)
-            f_result *= zwischen
+            # multiply the individual propability to the total propability
+            f_result *= i_count / len(df_subdata)
 
         return f_result
--- 09_bayes_homework/main.py
+++ 09_bayes_homework/main.py
@@ -1,31 +1,70 @@
+"""
+using a naive bayes classificator to predict diseases
+in the acute inflammation dataset
+"""
 from pathlib import Path
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 from bayes import NaiveBayes
 
+# path to the dataset
 PTH_DATA_FILE = Path("diagnosis.data")
+# encoding of the dataset
 S_CSV_ENCODING = "utf_16_le"
+# value-delimiter of the csv-dataset
 CHR_CSV_DELIMITER = '\t'
+# decimal-point of the dataset
 CHR_CSV_DECIMAL_POINT = ','
+# split between training and test data
 F_TRAINING_TEST_SPLIT = 0.8
+# value for the alpha value of the laplace factor
 F_LAPLACE_ALPHA = 1
 
+# treshold, at which a result should be true
 F_TRESHOLD_ERROR = 0.5
 
-TPL_COLUMN_NAMES = ("Temperature of patient", "Occurrence of nausea", "Lumbar  ain", "Urine pushing", "Micturition pains", "Burning of urethra, itch, swelling of urethra outlet", "Inflammation of urinary bladder", "Nephritis of renal pelvis origin")
+# names of the csv-columns
+TPL_COLUMN_NAMES = (
+	"Temperature of patient",
+	"Occurrence of nausea",
+	"Lumbar  ain",
+	"Urine pushing",
+	"Micturition pains",
+	"Burning of urethra, itch, swelling of urethra outlet",
+	"Inflammation of urinary bladder",
+	"Nephritis of renal pelvis origin"
+)
+# names of the disease-columns
 TPL_DISEASES = ("Inflammation of urinary bladder", "Nephritis of renal pelvis origin")
 
-TPL_F_TEMP_BOUNDS = (35.0, 36.5, 37.4, 42.0)
+# bounds for the temperature-classes
+TPL_F_TEMP_BOUNDS = (35.0, 36.5, 38, 42.0)
+# names of the temperature classes
 TPL_S_TEMP_LABELS = ("cold", "normal", "hot")
 
 def main():
+    """main-function
+    """
     # load the dataset into a pandas dataframe
-    df_data = pd.read_csv(PTH_DATA_FILE, delimiter=CHR_CSV_DELIMITER, names=TPL_COLUMN_NAMES, decimal=CHR_CSV_DECIMAL_POINT, encoding=S_CSV_ENCODING)
-    df_data: pd.DataFrame = df_data.map(yes_no_map)
+    df_data = pd.read_csv(
+        filepath_or_buffer=PTH_DATA_FILE,
+        delimiter=CHR_CSV_DELIMITER,
+        names=TPL_COLUMN_NAMES,
+        decimal=CHR_CSV_DECIMAL_POINT,
+        encoding=S_CSV_ENCODING,
+        true_values=["yes"],
+        false_values=["no"]
+    )
 
-    df_data["Temperature of patient"] = pd.cut(df_data["Temperature of patient"], bins=TPL_F_TEMP_BOUNDS, include_lowest=True, labels=TPL_S_TEMP_LABELS)
-
-    # df_data.drop("Temperature of patient", axis=1)
+    # map the temperature into the classes
+    df_data["Temperature of patient"] = pd.cut(
+        df_data["Temperature of patient"],
+        bins=TPL_F_TEMP_BOUNDS,
+        include_lowest=True,
+        labels=TPL_S_TEMP_LABELS
+    )
 
     # split the data into the training- and test-data
     df_training, df_test = df_training_test_split(df_data, F_TRAINING_TEST_SPLIT)
@@ -33,25 +72,85 @@ def main():
     # initialize the naive bayes
     nb_acute_inflammation = NaiveBayes(df_training, list(TPL_DISEASES), F_LAPLACE_ALPHA)
 
-    for ii, sr_test in df_test.iterrows():
+    # create an empty numpy array to store the results
+    np_results = np.empty((len(df_test), 5))
+
+    # go throught the test-data and test every single dataset
+    for i_count, (i_index, sr_test) in enumerate(df_test.iterrows()):
+        # take only the symptoms
         sr_symptoms = sr_test.drop(list(TPL_DISEASES))
 
+        # print the index (= line-number) of the data in the csv
+        print(f"\nline {i_index}")
+
+        # dictionary to store the results of the current data
+        dct_res = {}
+
+        # go through all the diseases
         for str_disease in TPL_DISEASES:
+            # predigt the propability of the current disease given the symptoms
             f_res = nb_acute_inflammation.f_predict(str_disease, sr_symptoms)
 
-            if (f_res >= F_TRESHOLD_ERROR) ^ sr_test[str_disease]:
-                print(f"{sr_test.name} {str_disease} {f_res:.4f} {sr_test[str_disease]}")
+            print (
+                f"{str_disease}: {f_res * 100:.1f} %"
+                f"-> {f_res >= F_TRESHOLD_ERROR} ({sr_test[str_disease]})"
+            )
 
-def yes_no_map(datapoint):
-    match datapoint:
-        case "yes":
-            return True
-        case "no":
-            return False
-        case _:
-            return datapoint
+            # store the results in the dictionary of the current disease
+            dct_res[str_disease] = (sr_test[str_disease], f_res)
 
-def df_training_test_split(df_data: pd.DataFrame, f_split: float) -> tuple[pd.DataFrame, pd.DataFrame]:
+        # store the current diseases results in the results-array
+        np_results[i_count] = (i_index, *dct_res[TPL_DISEASES[0]], *dct_res[TPL_DISEASES[1]])
+
+    # construct the header of the results-dataframe
+    tpl_header_values = ("reference", "propability")
+    tpl_header_diseases = (x for x in TPL_DISEASES for _ in range(len(tpl_header_values)))
+    tpl_header = tuple(x for x in zip(tpl_header_diseases, tpl_header_values * len(TPL_DISEASES)))
+
+    # store the results in a dataframe
+    df_results = pd.DataFrame(
+        np_results[:, 1:],
+        index=np_results[:, 0],
+        columns=pd.MultiIndex.from_tuples(tpl_header, names=("disease", "values"))
+    )
+
+    # change the result-columns into booleans
+    for dd in TPL_DISEASES:
+        df_results[dd, "reference"] = df_results[dd, "reference"].astype(bool)
+
+    # change the index into an integer
+    df_results.index = df_results.index.astype(int)
+
+    # create a subplot to show all the plots at once
+    axs = plt.subplots(len(TPL_DISEASES), 2)[1]
+
+    # create plots for every disease
+    for xx, dd in enumerate(TPL_DISEASES):
+        # create a plot for the two possible results
+        for yy, rr in enumerate((False, True)):
+            # select only the data of the current disease and result
+            df_sub = df_results[df_results[dd, "reference"] == rr]
+
+            # ticks for the x-axis
+            x_ticks = np.arange(len(df_sub))
+            # create the plot
+            axs[xx, yy].bar(x_ticks, height=df_sub[dd, "propability"])
+
+            # format the plot
+            axs[xx, yy].set_xticks(x_ticks)
+            # set the labels of x-axis to the column numbers
+            axs[xx, yy].set_xticklabels(df_sub.index)
+            axs[xx, yy].set_ylim((0, 1))
+            axs[xx, yy].title.set_text(f"{dd} = {rr}")
+            axs[xx, yy].set_xlabel("index")
+            axs[xx, yy].set_ylabel("propability")
+
+    plt.show()
+
+def df_training_test_split(
+        df_data: pd.DataFrame,
+        f_split: float
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
     """Split a dataframe into a training- and test-dataframe
 
     Args:
@@ -61,7 +160,9 @@ def df_training_test_split(df_data: pd.DataFrame, f_split: float) -> tuple[pd.Da
     Returns:
         tuple[pd.DataFrame, pd.DataFrame]: training-dataframe, test-dataframe
     """
+    # randomly select the training-data
     df_training = df_data.sample(frac=f_split)
+    # use the remaining data as the test-data
     df_test = df_data.drop(df_training.index)
 
     return df_training, df_test
